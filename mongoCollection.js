c3b6a77b88a832577667f17836235495adc51ca9
i-Mhyke <58915377+i-Mhyke@users.noreply.github.com>
2020-03-30 23:29:15 +0100

    Update mongoCollection.js

--- mongoCollection.js
+++ mongoCollection.js
@@ -3,7 +3,6 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/i_Mhyke';
 
 MongoClient.connect(url, (err, db) =>{
-	 useUnifiedTopology: true
   if (err) throw err;
   let database = db.db("i_Mhyke");
   database.createCollection("interns", (err, res)=> {
